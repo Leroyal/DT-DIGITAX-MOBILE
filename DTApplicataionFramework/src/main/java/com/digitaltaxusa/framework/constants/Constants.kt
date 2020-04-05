55291d990322d1c1b3a3ddf3ce2924ffdaa23d79
drxeno02 <ljtatum@hotmail.com>
2020-04-05 01:21:04 -0700

    :robot: DTUA-21 minor updates

--- DTApplicataionFramework/src/main/java/com/digitaltaxusa/framework/constants/Constants.kt
+++ DTApplicataionFramework/src/main/java/com/digitaltaxusa/framework/constants/Constants.kt
@@ -5,6 +5,8 @@ import com.digitaltaxusa.framework.BuildConfig
 object Constants {
     // debuggable mode; true to see debug logs otherwise false
     const val DEBUG = BuildConfig.DEBUG_MODE
+    // verbose mode; true to see verbosity mode otherwise false
+    const val DEBUG_VERBOSE = true
     // unique identifier
     const val ANDROID = "android"
     const val KEY_ANDROID_ID = "androidId"
--- DTApplicataionFramework/src/main/java/com/digitaltaxusa/framework/utils/FrameworkUtils.kt
+++ DTApplicataionFramework/src/main/java/com/digitaltaxusa/framework/utils/FrameworkUtils.kt
@@ -1,16 +1,22 @@
 package com.digitaltaxusa.framework.utils
 
+import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Handler
 import android.os.SystemClock
+import android.telephony.TelephonyManager
 import android.text.util.Linkify
+import android.util.DisplayMetrics
 import android.util.Patterns
 import android.view.View
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import com.digitaltaxusa.framework.BuildConfig
+import com.digitaltaxusa.framework.constants.Constants
+import com.digitaltaxusa.framework.logger.Logger
 import java.text.DecimalFormat
 import java.text.ParseException
 import java.text.SimpleDateFormat
@@ -25,6 +31,89 @@ object FrameworkUtils {
     // click control threshold
     private const val CLICK_THRESHOLD = 300
     private var lastClickTime: Long = 0
+
+    // tags
+    // used for app debugging
+    private const val TAG_MEMORY = "debug-memory"
+    private const val TAG_INFO = "debug-info"
+
+    // device attributes
+    // used for app debugging
+    private const val OS_VERSION = "os.version"
+
+    /**
+     * Method is used for printing the memory usage. This is used
+     * only for verbosity mode
+     *
+     * @param name fragment or class simple name
+     */
+    fun printMemory(name: String) {
+        if (Constants.DEBUG && Constants.DEBUG_VERBOSE) {
+            val totalMemory = Runtime.getRuntime().totalMemory()
+            val freeMemory = Runtime.getRuntime().freeMemory()
+            val usedMemory = totalMemory - freeMemory
+            // note that you cannot divide a long by a long value, this
+            // refers to (long/long - long) operation giving a long result of 0
+            val percentFree = (freeMemory.toFloat() / totalMemory * 100).toLong()
+            val percentUsed = (usedMemory.toFloat() / totalMemory * 100).toLong()
+            if (percentFree <= 2) {
+                Logger.e(
+                    TAG_MEMORY, "===== MEMORY WARNING :: Available memory is low! Please add " +
+                            "'art-' to your regex tag to see that gc is freeing up more available memory ====="
+                )
+            }
+            // printing memory details
+            Logger.d(
+                TAG_MEMORY, "===== Memory recorded from " + name + " :: " +
+                        "MAX_MEMORY:" + Runtime.getRuntime().maxMemory() +
+                        "  // FREE_MEMORY:" + freeMemory + " (" + percentFree + "% free)" +
+                        "  // TOTAL_MEMORY:" + totalMemory +
+                        "  // USED_MEMORY:" + usedMemory + " (" + percentUsed + "% used) ====="
+            )
+        }
+    }
+
+    /**
+     * Method is used to print device and application information. This is
+     * used only for verbosity mode
+     *
+     * @param context  Interface to global information about an application environment
+     * @param activity The in-memory representation of a Java class
+     */
+    fun printInfo(context: Context, activity: Activity) {
+        if (Constants.DEBUG && Constants.DEBUG_VERBOSE) {
+            // determine phone carrier
+            val manager = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+            val carrierName = manager.networkOperatorName
+            // get display metrics
+            val displayMetrics = DisplayMetrics()
+            activity.windowManager.defaultDisplay.getMetrics(displayMetrics)
+            try {
+                Logger.i(
+                    TAG_INFO, "===== DEVICE INFORMATION =====" +
+                            "\nManufacturer: " + Build.MANUFACTURER +
+                            "\nRegistrationModel: " + Build.MODEL +
+                            "\nDevice/Product Id: " + Build.PRODUCT +
+                            "\nCarrier: " + carrierName +
+                            "\nOS Version: " + System.getProperty(OS_VERSION) +
+                            "\nAPI Level: " + Build.VERSION.SDK_INT +
+                            "\nScreen size (width/height): " +
+                            displayMetrics.widthPixels + "/" +
+                            displayMetrics.heightPixels +
+                            "\n===== APP INFORMATION =====" +
+                            "\nApp Version: " + BuildConfig.VERSION_NAME +
+                            "\nBuild Type: " + BuildConfig.BUILD_TYPE +
+                            "\nVersion Code: " + BuildConfig.VERSION_CODE +
+                            "\nPackage Name: " + context.packageName +
+                            "\nFlavor: " + BuildConfig.FLAVOR +
+                            "\nGoogle Map API Version: " + context.packageManager
+                        .getPackageInfo("com.google.android.apps.maps", 0).versionName
+                )
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
 
     /**
      * Method is used to Linkify words in a TextView
@@ -274,12 +363,9 @@ object FrameworkUtils {
         val endDate: Date
         var numberOfDays: Long = 0
         try {
-            startDate = formatter.parse(dateA)
-            endDate = formatter.parse(dateB)
-            var timeDiff: Long = 0
-            if (endDate != null && startDate != null) {
-                timeDiff = endDate.time - startDate.time
-            }
+            startDate = formatter.parse(dateA) ?: Date()
+            endDate = formatter.parse(dateB) ?: Date()
+            val timeDiff: Long = endDate.time - startDate.time
             numberOfDays = TimeUnit.DAYS.convert(timeDiff, TimeUnit.MILLISECONDS)
         } catch (e: ParseException) {
             e.printStackTrace()
