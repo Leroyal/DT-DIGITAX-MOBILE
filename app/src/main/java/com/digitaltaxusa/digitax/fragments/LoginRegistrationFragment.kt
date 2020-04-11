90855c14b157ec7b4741427df0099d784273d063
drxeno02 <ljtatum@hotmail.com>
2020-04-11 09:53:33 -0700

    :robot: DTUA-15 preparation for fingerprint authentication part 3 of 4

---
+++ app/src/main/java/com/digitaltaxusa/digitax/fragments/LoginRegistrationFragment.kt
@@ -0,0 +1,13 @@
+package com.digitaltaxusa.digitax.fragments
+
+import android.view.View
+
+class LoginRegistrationFragment : BaseFragment(), View.OnClickListener {
+
+
+    override fun onClick(v: View) {
+        TODO("Not yet implemented")
+    }
+
+
+}
