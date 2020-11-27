b0e9b0c4db76e14685d4fe29ed5c3c87db3be11d
drxeno02 <ljtatum@hotmail.com>
2020-11-27 12:41:47 -0800

    :construction: DTUA-40 created request adapter

--- app/src/main/java/com/digitaltaxusa/digitax/api/client/internal/adapter/DtoAdapter.kt
+++ app/src/main/java/com/digitaltaxusa/digitax/api/client/internal/adapter/DtoAdapter.kt
@@ -1,7 +1,9 @@
 package com.digitaltaxusa.digitax.api.client.internal.adapter
 
 import com.digitaltaxusa.digitax.api.client.internal.model.SigninRequestDto
+import com.digitaltaxusa.digitax.api.client.internal.model.SignupRequestDto
 import com.digitaltaxusa.digitax.api.requests.SigninRequest
+import com.digitaltaxusa.digitax.api.requests.SignupRequest
 
 /**
  * Allows the interface of an existing class [SigninRequest] to be used as another
@@ -10,7 +12,7 @@ import com.digitaltaxusa.digitax.api.requests.SigninRequest
  * <p>This pattern is often used to make existing classes work with others without modifying
  * their source code.</p>
  *
- * @param signinRequest SigninRequest Object for marking /api/auth/signin request.
+ * @param signinRequest [SigninRequest] object for making /api/auth/signin request.
  * @return SigninRequestDto
  */
 internal fun adapt(signinRequest: SigninRequest) = SigninRequestDto(
@@ -18,4 +20,22 @@ internal fun adapt(signinRequest: SigninRequest) = SigninRequestDto(
     email = signinRequest.email,
     password = signinRequest.password,
     username = signinRequest.username
+)
+
+/**
+ * Allows the interface of an existing class [SignupRequest] to be used as another
+ * interface [SignupRequestDto].
+ *
+ * <p>This pattern is often used to make existing classes work with others without modifying
+ * their source code.</p>
+ *
+ * @param signupRequest [SignupRequest] object for making /api/auth/signup request.
+ * @return SignupRequestDto
+ */
+internal fun adapt(signupRequest: SignupRequest) = SignupRequestDto(
+    deviceType = signupRequest.deviceType,
+    email = signupRequest.email,
+    password = signupRequest.password,
+    username = signupRequest.username,
+    role = signupRequest.role
 )
--- app/src/main/java/com/digitaltaxusa/digitax/api/client/internal/model/SignupRequestDto.kt
+++ app/src/main/java/com/digitaltaxusa/digitax/api/client/internal/model/SignupRequestDto.kt
@@ -7,14 +7,16 @@ import com.google.gson.annotations.SerializedName
  * transferred over the network.
  *
  * @property deviceType String? String The type of device used for the API call.
- * @property email String? String Email required for /api/auth/signin request.
- * @property password String? String Password required for /api/auth/signin request.
- * @property username String? String Username required for /api/auth/signin request.
+ * @property email String? String Email required for /api/auth/signup request.
+ * @property password String? String Password required for /api/auth/signup request.
+ * @property username String? String Username required for /api/auth/signup request.
+ * @property role List<String> List of specific roles for admin account creation.
  * @constructor
  */
-internal data class SigninRequestDto(
+internal data class SignupRequestDto(
     @SerializedName("deviceType") val deviceType: String?,
     @SerializedName("email") val email: String?,
     @SerializedName("password") val password: String?,
-    @SerializedName("username") val username: String?
+    @SerializedName("username") val username: String?,
+    @SerializedName("role") val role: List<String> = mutableListOf()
 )
