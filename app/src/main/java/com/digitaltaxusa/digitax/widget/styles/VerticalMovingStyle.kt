5ae14abfeea2ed58a8da70f76eb90e4bbafafcd7
drxeno02 <ljtatum@hotmail.com>
2021-05-29 11:10:05 -0700

    :construction: DTUA-66 created vertical moving style and minor updates

--- app/src/main/java/com/digitaltaxusa/digitax/widget/styles/VerticalMovingStyle.kt
+++ app/src/main/java/com/digitaltaxusa/digitax/widget/styles/VerticalMovingStyle.kt
@@ -6,14 +6,14 @@ import com.digitaltaxusa.digitax.widget.ParallaxImageView
 import kotlin.math.abs
 
 /**
- * When the imageView is scrolling horizontally, the image in imageView will also
+ * When the imageView is scrolling vertically, the image in imageView will also
  * scroll horizontally if the image width is larger than the imageView width.
  *
  * Note: The image will not over scroll to it's view bounds.
  *
  * <p>Only supports imageView with CENTER_CROP scale type.</p>
  */
-class HorizontalMovingStyle : ParallaxImageView.ParallaxStyle {
+class VerticalMovingStyle : ParallaxImageView.ParallaxStyle {
 
     override fun onAttachedToImageView(view: ParallaxImageView?) {
         // only supports CENTER_CROP
@@ -28,7 +28,7 @@ class HorizontalMovingStyle : ParallaxImageView.ParallaxStyle {
         x: Int,
         y: Int
     ) {
-        var tempX = x
+        var tempY = y
         // only supports CENTER_CROP. If any other scale type return
         if (view?.scaleType !== ImageView.ScaleType.CENTER_CROP) {
             return
@@ -45,20 +45,20 @@ class HorizontalMovingStyle : ParallaxImageView.ParallaxStyle {
         val viewWidth: Int = view.width - view.paddingLeft - view.paddingRight
         val viewHeight: Int = view.height - view.paddingTop - view.paddingBottom
 
-        // device width
-        val deviceWidth: Int = view.resources.displayMetrics.widthPixels
-        if (imageWidth * viewHeight > imageHeight * viewWidth) {
+        // device height
+        val deviceHeight: Int = view.resources.displayMetrics.heightPixels
+        if (imageWidth * viewHeight < imageHeight * viewWidth) {
             // avoid over scroll
-            if (tempX < -viewWidth) {
-                tempX = -viewWidth
-            } else if (tempX > deviceWidth) {
-                tempX = deviceWidth
+            if (tempY < -viewHeight) {
+                tempY = -viewHeight
+            } else if (tempY > deviceHeight) {
+                tempY = deviceHeight
             }
-            val imageScale = viewHeight.toFloat() / imageHeight.toFloat()
-            val maxDx = abs((imageWidth * imageScale - viewWidth) * 0.5f)
-            val translateX = -(2 * maxDx * tempX + maxDx * (viewWidth - deviceWidth)) /
-                    (viewWidth + deviceWidth)
-            canvas?.translate(translateX, 0f)
+            val imageScale = viewWidth.toFloat() / imageWidth.toFloat()
+            val maxDy = abs((imageHeight * imageScale - viewHeight) * 0.5f)
+            val translateY = -(2 * maxDy * tempY + maxDy * (viewHeight - deviceHeight)) /
+                    (viewHeight + deviceHeight)
+            canvas?.translate(0f, translateY)
         }
     }
 }
